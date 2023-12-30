2388208c2b0f3ce76262b7af2db6a5802b27a3e5
unknown <ODuha>
2023-12-30 16:05:50 +0100

    updated

--- resources/views/Professeur/InfoProfesseur.blade.php
+++ resources/views/Professeur/InfoProfesseur.blade.php
@@ -1,41 +1,121 @@
 @extends('Professeur.Sidebar')
 @section('navsidebarProf')
-    <p class="text-upperace text-danger fs-1">saybi index deyal navigation</p>
     <div class="container">
-        <div class="card" >
+        <div class="card shadow" style=" width: 800px;
+  margin: auto;
+  padding: 20px 24px; background: #ffffff4a;" >
             <div class="card-body">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6 col-xl-6">
-                        <label for="">Nom</label>
-                        <input type="text" class="form-control" placeholder="nom" value="{{$DataProfesseur->nom}}">
+                    <div class="picture-container">
+                        <div class="picture">
+                            <img src="{{ $DataProfesseur->image == '' ? asset('image/default-avatar.png') : $DataProfesseur->image}}" class="picture-src" id="wizardPicturePreview" title=""/>
+                            <input type="file" id="wizard-picture" name="image">
+                        </div>
 
-                        <label for="">Email</label>
-                        <input type="email" class="form-control" placeholder="nom" value="{{$DataProfesseur->email}}">
-
-                        <label for="">Pays</label>
-                        <input type="text" class="form-control" placeholder="nom" value="{{$DataProfesseur->pays}}">
-
-                        <label for="">Titre</label>
-                        <input type="text" class="form-control" placeholder="nom" value="{{$DataProfesseur->title}}">
-
-                        <label for="">img</label>
-                        <img src="{{$DataProfesseur->image}}" class="w-25" alt="">
                     </div>
-                    <div class="col-sm-12 col-md-6 col-xl-6">
-                        <label for="">Prénom</label>
-                        <input type="text" class="form-control" placeholder="Prénom" value="{{$DataProfesseur->prenom}}">
-
-                        <label for="">télephone</label>
-                        <input type="text" class="form-control" placeholder="Prénom" value="{{$DataProfesseur->telephone}}">
-
-                        <label for="">Date naissance</label>
-                        <input type="date" class="form-control" placeholder="Prénom" value="{{$DataProfesseur->datenaissance}}">
-
-                        <label for="">Méthode</label>
-                        <textarea name="" id="" class="form-control"  rows="3">{{$DataProfesseur->description}}</textarea>
+                    <div class="col-sm-12 col-md-6 col-xl-6 mt-5">
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">Nom complet</label>
+                            <input type="text" class="form-control" placeholder="nom" value="{{$DataProfesseur->nom}}">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">télephone</label>
+                            <input type="text" class="form-control" placeholder="Prénom" value="{{$DataProfesseur->telephone}}">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">Titre</label>
+                            <input type="text" class="form-control" placeholder="nom" value="{{$DataProfesseur->title}}">
+                        </div>  
+                     
                     </div>
-                </div>
+                    <div class="col-sm-12 col-md-6 col-xl-6 mt-5">
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">Email</label>
+                            <input type="email" class="form-control" placeholder="nom" value="{{$DataProfesseur->email}}">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">Date naissance</label>
+                            <input type="date" class="form-control" placeholder="Prénom" value="{{$DataProfesseur->datenaissance}}">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="" class="mb-1">Méthode</label>
+                            <textarea name="" id="" class="form-control"  rows="3">{{$DataProfesseur->description}}</textarea>
+                        <div>
+                    </div>
+                </div> 
+            </div>
+            <div class="mt-5">
+               <button type="button" class="btn btn-success  BtnUpdateDataProfesseur" style="display:flex; margin:auto">Valider</button>
             </div>
         </div>
     </div>
+
+    <style>
+        .picture-src{
+            width: 160px;
+  height: 160px;
+ 
+        }
+
+        .form-control:focus{
+            border-bottom:1px solid black !important;
+            box-shadow: none;
+        }
+        .form-control{
+        border-radius: 0px;}
+
+        .picture-container {
+  position: relative;
+  cursor: pointer;
+  text-align: center;
+}
+ .picture {
+  width: 160px;
+  height: 160px;
+  background-color: #999999;
+  border: 4px solid #CCCCCC;
+  color: #FFFFFF;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: all 0.2s;
+  -webkit-transition: all 0.2s;
+  margin:auto;
+}
+.picture:hover {
+  border-color: #4479a7;
+}
+.picture input[type="file"] {
+  cursor: pointer;
+  display: block;
+  height: 100%;
+  left: 0;
+  opacity: 0 !important;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+
+
+    </style>
+
+    <script>
+$(document).ready(function ()
+{
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#wizard-picture").change(function(){
+        readURL(this);
+    });
+});
+    </script>
 @endsection
