d6bc68350afa8791e5f01eaa54971144110dfa6c
unknown <ODuha>
2024-01-22 00:19:43 +0100

    upUp

--- resources/views/Eleve/Details.blade.php
+++ resources/views/Eleve/Details.blade.php
@@ -1,4 +1,223 @@
 @extends('layouts.app')
-
 @section('content')
+<link rel="stylesheet" href="{{asset('css/StyleProfileProf.css')}}">
+<script src="{{asset('js/ScriptShowProfile.js')}}"></script>
+<div class="container">
+    <div class="row mt-5">
+        <div class="card text-left mt-5 mb-5" style="">
+        <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <div class="card-body">
+                <div class="titreImage">
+                <h4 class="card-title ">Cours "G/P" de "Nom"</h4>
+                <img src="{{asset('image/default-avatar.png') }}" class="avatar" alt="" >
+                </div>
+                <p class="card-text-label mt-5"> 
+                    <div class="row">
+                        <div class="col-sm-12 col-md-3 col-xl-3 ">
+                        <label> <i class="fa fa-book fa-xl" aria-hidden="true"></i>  cours1 </label>        
+                        </div>
+                        <div class="col-sm-12 col-md-3 col-xl-3">
+                        <label> <i class="fa fa-clock fa-xl" aria-hidden="true"></i>  Dimanche, 21 janvier 2024 <span> 19:00 - 20:00 (GMT+1)</span> </label>        
+                        </div>
+                        <div class="col-sm-12 col-md-3 col-xl-3 ">
+                        <label> <i class="fa fa-users  fa-xl" aria-hidden="true"></i>  2 Participants</label> 
+                        </div>
+                        <div class="col-sm-12 col-md-3 col-xl-3 ">
+                        <label> <i class="fa fa-video-camera fa-xl" aria-hidden="true"></i>  Hébergé sur Zoom</label> 
+                        </div>
+                    </div>
+                     </p>
+            
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Reserver</a>
+            </div>
+        </div>
+        <div class="row row-card " style="background: #ffffff5c; display:flex; margin:auto ; padding-bottom:36px">
+        <h3 class="mt-5 mb-4">A propos "Nom"</h3>
+            <div id="divProfile" class="col-12">
+                <div class="card text-center">
+                    <img class=" prof-image mt-5" src="{{asset('image/default-avatar.png') }}" alt="Profile image"  srcset="" >
+                    <h2 class="name">Ouardi douha</h2>
+                    <h4 >Prof d'arabe</h4>
+                    <h4 ><i class="fa fa-briefcase" aria-hidden="true" ></i> <span>4 </span> ans d'expérience</h4>
+                    <div class="card-body" >
+                        <p class="card-text">description de description</p>
+                    </div>
+                </div>
+            </div>
+            <div id="divDispo" class="col-8 mt-5">
+                <div class="card cardDispo text-left">
+                    <h4 class="title-card"  style="  border-bottom: 1px solid #c0c1c1;">Disponibilite de professeur </h4>
+                    <div class="card-body ">
+                        <div class="ClassDisponible">
+                        {{-- @foreach ($disponibilityByDay as $key => $data)
+                            <div class="ContentDisponible">
+                                <div class="Days">{{$key}}</div>
+                                    <div class="ClassCalculHeight" >
+                                        <div class="ClassTimeDisponible"  style="@if($data) color:#0c3c74;background:#00f8ff3b; @else '' @endif">
+                                            <p>
+                                                @if($data)
+                                                    <i class="fa-solid fa-clock" style="color: #0078ff"></i>
+                                                @endif
+                                                {{$data ? $data->debut : "Vide"}}
+                                            </p>
+                                            <p>
+                                                @if($data)
+                                                    <i class="fa-solid fa-clock" style="color: #0078ff"></i>
+                                                    @endif
+                                                {{$data ? $data->fin : 'Vide'}}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                        @endforeach --}} {{-- mate9isich had Code li dayr lih comment --}}
+                            @foreach ($disponibilityByDay as $key => $data)
+
+                                <div class="ContentDisponible" >
+                                    <div class="Days">{{$key}}</div>
+                                    <div class="ClassCalculHeight">
+                                        @foreach($data as $item)
+                                            <div class="ClassTimeDisponible" style="color:#0c3c74;background:#00f8ff3b;">
+                                                <p>
+                                                    <i class="fa-solid fa-clock" style="color: #0078ff"></i>
+                                                    {{$item->debut}}
+                                                </p>
+                                                <p>
+                                                    <i class="fa-solid fa-clock" style="color: #0078ff"></i>
+                                                    {{$item->fin}}
+                                                </p>
+                                            </div>
+                                        @endforeach
+                                        @if(empty($data))
+                                            <div class="ClassTimeDisponible" style="color: #0c3c74;background: #00f8ff3b;">
+                                                <p>Vide</p>
+                                                <p>Vide</p>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="divCours" class="col-4 mt-5">
+                <div class="card cardCours">
+                    <h4 class="title-card"  style="  border-bottom: 1px solid #c0c1c1;">Les cours </h4>
+                    <div class="card-body">
+                        <div class="divScrollCours" >
+                            @foreach ($CourProf as $item)
+                                <p class="card-cours">{{$item->title}}</p>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="divFormation" class="col-6 mt-5">
+                <div class="card cardFormation" >
+                    <h4 class="title-card "  style="  border-bottom: 1px solid #c0c1c1;"> Formation</h4>
+                        <div class="card-body timeline" >
+                            <ul class="list-unstyled " id="ListFormation">
+                                @foreach ($FormationProf as $key => $item)
+                                    <div class="item" data-index="{{$key}}" style="{{$key > 0 ? 'display:none;' : ''}}" {{-- {{ $key >= 1 ? 'hidden' : '' }}" --}}>
+                                        <li class="timeline-item" >
+                                            <div class="timeline-element">
+                                                <a href="#">{{ $item->diplome }} </a>
+                                                <span class="date">{{ \Carbon\Carbon::createFromFormat('Y-m-d', $item->annee)->format('m/Y') }}</span>
+                                                <span class="pays">{{ $item->pays }}</span>
+                                                <span class="circle"></span>
+                                                <div class="timeline-content">
+                                                    <p>{{ $item->specialise }}</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </div>
+                                @endforeach
+                            </ul>
+                        </div>
+                    <div class="show"  >
+                        <button id="showMoreFormation" class="btn showMore">Voir plus</button>
+                        <button id="showFirstItemFormation" class="btn showLess " style="display: none">Voir moins</button>
+                    </div>
+                </div>
+            </div>
+            <div id="divExperience" class="col-6 mt-5">
+                <div class="card cardExperience" >
+                    <h4 class="title-card "  style="  border-bottom: 1px solid #c0c1c1;"> Experience</h4>
+                    <div class="timeline">
+                        <ul id="ListExperince">
+                            @foreach ($ExperinceProf as $key => $item)
+                                <div class="item" data-index="{{ $key }}" style="{{ $key > 0 ? 'display:none;' : '' }}">
+                                    <li>
+                                        <span>{{ \Carbon\Carbon::createFromFormat('Y-m-d', $item->du)->format('m/Y') }}
+                                            <span style="padding-left: 4px;padding-right: 4px;">-</span>
+                                            {{ \Carbon\Carbon::createFromFormat('Y-m-d', $item->au)->format('m/Y') }}
+                                        </span>
+                                        <div class="timeline-content">
+                                            <h3>{{ $item->poste}}</h3>
+                                            <p>{{ $item->entreprise}} /{{ $item->pays}}</p>
+                                        </div>
+                                    </li>
+                                </div>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="show">
+                        <button id="showMoreExperience" class="btn showMore">Voir plus</button>
+                        <button id="showFirstItemExperience" class="btn showLess " style="display: none">Voir moins</button>
+                    </div>
+                </div>
+            </div>
+
+           
+        </div>
+ 
+                    
+    </div>
+</div>
+<style>
+    .card-text-label label{
+        display:block;
+        margin-bottom:20px
+    }
+    i{
+        margin-right:15px
+    }
+    h4 {display:inline}
+    .avatar{
+        width: 65px;
+        height: 65px;
+        float: left;
+        border-radius: 50%;
+        border: 1px solid gray;
+    }
+    .card-text span{
+        display: block;
+         margin-left: 42px;
+    }
+    .prof-image{
+        width: 90px;
+        height: 90px;
+        border-radius: 50%;
+        border: 1px solid gray;
+        text-align:center;
+        display:flex;
+        margin:auto
+       
+    }
+    .row-card{
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      overflow: hidden;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+    .titreImage{
+        display: flex;
+        justify-content: space-evenly;
+        align-content: center;
+        align-items: center;
+        }
+    }
+ 
+</style>
 @endsection
